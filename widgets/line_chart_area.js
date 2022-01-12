4edf6a7c72c1762888024c4a252f9b8118fa8393
mdiakhate12 <mdiakhate1297@gmail.com>
2022-01-12 08:48:32 +0100

    Mouhammad Diakhaté's Viz

--- widgets/line_chart_area.js
+++ widgets/line_chart_area.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-  width = 660 - margin.left - margin.right,
+  width = 760 - margin.left - margin.right,
   height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -10,6 +10,10 @@ const svg = d3.select("#line_chart_area")
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
+
+d3.csv("/data/clean_sleep_data.csv", d => {
+  console.log(d)
+})
 
 //Read the data
 d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
